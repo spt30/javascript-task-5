81f6bf3df71fb2c7db26212c459d0796a75fab8c
spt30 <seregatoschilin@gmail.com>
2017-10-30 07:53:28 +0500

    Первая попытка

--- emitter.js
+++ emitter.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
 
 /**
@@ -13,32 +13,96 @@ module.exports = getEmitter;
  */
 function getEmitter() {
     return {
+        subscriptions: [],
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            console.info('ON', event, context, handler);
+            this.subscriptions.push({ e: event, c: context, h: handler });
+            console.info(this);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            console.info('OFF', event, context);
+            // console.info(this.subscriptions.e);
+            let deleteList = [];
+
+            this.subscriptions.forEach(function (sub, i) {
+                console.info('OFF1', sub);
+                let eventNum = sub.e.indexOf(event);
+                console.info('OFF2', eventNum);
+                if (eventNum === 0) {
+                    if (sub.e[eventNum + event.length] !== '.' &&
+                    sub.e[eventNum + event.length] !== undefined) {
+                        console.info('OFF3', sub.e[eventNum + event.length]);
+
+                        return 0;
+                    }
+                    if (context === sub.c && eventNum === 0) {
+                        console.info('111');
+                        console.info(deleteList);
+                        deleteList.push(i);
+                        console.info(deleteList);
+                    }
+                }
+            }, this);
+
+            let i = 0;
+            console.info(this.subscriptions.length);
+            console.info(deleteList);
+            while (i < this.subscriptions.length) {
+                if (deleteList.indexOf(i) + 1) {
+                    console.info('11', i);
+                    this.subscriptions.splice(i, 1);
+                    deleteList.shift();
+                } else {
+                    i++;
+                }
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            console.info('EMIT', event);
+            let eventList = [event];
+            if (event.indexOf('.') + 1) {
+                let eventLeft = event.split('.');
+                console.info('EMIT1', eventLeft);
+                eventList.push(eventLeft[0]);
+                console.info('EMIT3', eventList);
+            }
+            eventList.forEach(function (e) {
+                console.info(e);
+                this.subscriptions.forEach(function (sub) {
+                    console.info(sub);
+                    if (sub.e === e) {
+                        console.info('yep');
+                        sub.h.call(sub.c);
+                    }
+                });
+            }, this);
+
+            return this;
         },
 
         /**
